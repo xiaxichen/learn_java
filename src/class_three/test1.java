ae361acf63c8071a42816a78c1768e4299b8fcd1
XiaXiChen <xiaxichen1@163.com>
2019-06-16 14:34:00 +0800

    添加测试文件

--- src/class_three/test1.java
+++ src/class_three/test1.java
@@ -1,7 +1,21 @@
 package class_three;
 
+//重载 overloading
 public class test1 {
-    public static void main(String[] args){
-//        boolean bool = isRunNian()
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int num = add(a, b);
+        System.out.println(num);
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static float add(float a, float b) {
+        return a + b;
+    }
+
 }
+
