c989445cff5c3a71580c0b3a20d7f8fe44a8baa2
XiaXiChen <xiaxichen1@163.com>
2019-06-16 13:57:38 +0800

    添加输入代码

---
+++ src/class_three/test1.java
@@ -0,0 +1,7 @@
+package class_three;
+
+public class test1 {
+    public static void main(String[] args){
+//        boolean bool = isRunNian()
+    }
+}
--- src/class_two/func.java
+++ src/class_two/func.java
@@ -5,39 +5,49 @@ import java.util.Scanner;
 
 public class func {
     public static void main(String[] args) {
-        getMenu();
-        System.out.println("请输入数字");
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-//        prtintStar(num);
-        System.out.println(isRunYear(num));
-
-    }
-   // public 访问权限修饰符
-   // static 静态修饰符，描述的方法可以直接调用
-   // void 返回值(无返回值)
-   public static void getMenu(){
-       System.out.println("--菜单--");
-       System.out.println("1,宫保鸡丁");
-       System.out.println("2,韭菜鸡蛋");
-       System.out.println("3,番茄炒蛋");
-       System.out.println("4,扬州炒饭");
-    }
-    public static void prtintStar(int num){
-        for (int i = 0; i <num; i++) {
-            for (int j = 0; j <i ; j++) {
-                System.out.print("✨");
+        while (true) {
+            getMenu();
+            System.out.println("请输入数字 退出输入exit");
+            Scanner input = new Scanner(System.in);
+            if (input.hasNextInt()) {
+                int num = input.nextInt();
+                //        System.out.println(isRunYear(num));
+                System.out.println(getMenu(num));
             }
-            System.out.println();
+            if (input.hasNext()) {
+                String instruction = input.next();
+                if (instruction == "exit") {
+                    break;
+                }
+            }
         }
+    }
 
+    // public 访问权限修饰符
+    // static 静态修饰符，描述的方法可以直接调用
+    // void 返回值(无返回值)
+    public static void getMenu() {
+        System.out.println("--菜单--");
+        System.out.println("1,宫保鸡丁");
+        System.out.println("2,韭菜鸡蛋");
+        System.out.println("3,番茄炒蛋");
+        System.out.println("4,扬州炒饭");
     }
-    public static String isRunYear(int year){
-        if((year%4==0 && year%100!=0) || year%400==0){
-            return "是闰年";
-        }else{
-            return "是平年";
+
+    public static String getMenu(int index) {
+        switch (index) {
+            case 1:
+                return "宫保鸡丁";
+            case 2:
+                return "韭菜鸡蛋";
+            case 3:
+                return "番茄炒蛋";
+            case 4:
+                return "扬州炒饭";
+            default:
+                return "没有这道菜";
         }
     }
+
 
 }
