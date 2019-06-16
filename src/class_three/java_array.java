b519a172623f1fcb4e62beb3fb92312ba2e85661
XiaXiChen <xiaxichen1@163.com>
2019-06-16 15:19:40 +0800

    添加笔记

--- src/class_three/java_array.java
+++ src/class_three/java_array.java
@@ -1,4 +1,4 @@
-package class_two;
+package class_three;
 
 
 import java.util.Arrays;
@@ -21,25 +21,33 @@ public class java_array {
         //第四种
         int[] num4 = {1, 2, 4, 5, 6};
         for_array(num4);
+        //循环遍历
+        int len = num3.length;
+        for(int i=0;i<len;i++){
+            int score = num3[i];
+            System.out.println(score);
+        }
+        System.out.println("------------------");
+        //forearch
+        for(int x:num3){
+            System.out.println(x);
+        }
+        System.out.println("------------------");
         print(1,2,3,4,5,6);
     }
 
     public static void for_array(int[] num_list) {
-//        int len = num_list.length;
-//        for (int i = 0; i < len; i++) {
-//            System.out.println(num_list[i]);
-//        }
         for (int x : num_list
         ) {
             System.out.println(x);
-            System.out.println("-----------------");
+            System.out.println("-----");
         }
     }
     public static void print(int i,int...num_list){
         for (int x : num_list
         ) {
             System.out.println(x);
-            System.out.println("-----------------");
+            System.out.println("---------");
         }
         System.out.println(num_list.length);
         System.out.println(i);
